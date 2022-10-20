51b5cca6c670988569aea8e53f0f58388eaad26c
mricoul <mricoul@beapi.fr>
2022-10-20 10:18:38 +0200

    chore (package): description and version

--- dist/index.js
+++ dist/index.js
@@ -2105,8 +2105,8 @@ Check the top-level render call using <` + c + ">.");
 const vt = ht.exports.jsx, Xt = /* @__PURE__ */ vt(Ht, {
   xmlns: "http://www.w3.org/2000/svg",
   viewBox: "0 0 24 24",
-  fill: "transparent",
   children: /* @__PURE__ */ vt(qt, {
+    fill: "transparent",
     stroke: "currentColor",
     strokeWidth: "1.75",
     d: "M9.25 14h-4.5a.75.75 0 0 0-.75.75v4.5c0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75v-4.5a.75.75 0 0 0-.75-.75Zm10.875 3a3.125 3.125 0 1 0-6.25 0 3.125 3.125 0 0 0 6.25 0ZM8.32 10.125 12 3.75l3.68 6.375H8.32Z"
