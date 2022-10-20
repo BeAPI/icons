6efba20b726e4dd760cdc141134355b79e3f370d
mricoul <mricoul@beapi.fr>
2022-10-20 10:08:46 +0200

    chore (vite.config): set config for npm package ready

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig( {
 	build: {
-		rollupOptions: {
-			output: {
-				entryFileNames: `assets/[name].js`,
-			},
+		lib: {
+			entry: resolve( __dirname, 'src/index.js' ),
+			name: '@beapi/icons',
+			fileName: 'index',
 		},
 	},
 	plugins: [ react( {
