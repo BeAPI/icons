c7dcddb7fb2266dcd14e5abd30d9cb70c3302fb5
mricoul <mricoul@beapi.fr>
2024-07-04 14:35:22 +0200

    feat (icons): update accordion icon

--- src/library/accordion.js
+++ src/library/accordion.js
@@ -5,7 +5,7 @@ import { SVG, Path } from '@wordpress/primitives';
 
 const accordion = (
 	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-		<Path d="M4 20a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2H4Zm0 0a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2H4Zm0-4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2H4ZM4 6a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2H4Zm5.75 6h-2v-1.5h2V12Zm3.5 0h-2v-1.5h2V12ZM20 10c0 1.1-.9 2-2 2v-1.5c.3 0 .5-.2.5-.5H20Zm0-1h-1.5V7H20v2ZM4 10h1.5c0 .3.2.5.5.5V12c-1.1 0-2-.9-2-2Zm0-3h1.5v2H4V7Zm12.75 5h-2v-1.5h2V12Z" />
+		<Path d="M18.5 5.5V8H20V5.5h2.5V4H20V1.5h-1.5V4H16v1.5h2.5ZM13.5 5.5H4V4h9.5v1.5ZM5.5 19v-4H7v4a.5.5 0 0 0 .5.5H18a.5.5 0 0 0 .5-.5v-4H20v4a2 2 0 0 1-2 2H7.5a2 2 0 0 1-2-2ZM4 13.5h9.5V12H4v1.5ZM22.5 12v1.5H16V12h6.5Z" />
 	</SVG>
 );
 
